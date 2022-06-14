6031510bb389416a07ca85ea5d67b61582441dee
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-14 23:29:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData(name,email){
+    
+   const data= fetch('http://localhost:3000/users',{
+        method:'POST',
+        headers:{
+            "Content-Type":"application/json",
+            Accept:"application/json"
+        },
+        body:JSON.stringify({name,email})
+    })
+    .then((res)=>res.json())
+    .then((data)=>{
+      document.body.innerHTML=data.id
+        
+    })
+    .catch((err)=>{
+        document.body.innerHTML=err.message
+      
+    })
+    return data;
+}
